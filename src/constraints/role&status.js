5952923f6289b91807c93df13827b252fecc11fd
nilpeep <niluferk038@gmail.com>
2024-04-16 22:10:48 +0300

    role and status

---
+++ src/constraints/role&status.js
@@ -0,0 +1,9 @@
+module.exports = {
+    role:{
+        1:"admin",
+        2:"staff",
+        3:"saler",
+        4:"purchaser",
+        5:"stockman"
+    }
+}
--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,36 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
-/* ------------------------------------------------------- */
+
+const { mongoose: {Schema, model} } = require('../configs/dbConnection')
+
+//? User Model:
+const UserSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+    password:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        index: true
+    },
+    role:{
+        type: Number,
+        required: true,
+        trim: true
+    }
+},{
+    collection:'users',
+    timestamps: true
+})
+
+
