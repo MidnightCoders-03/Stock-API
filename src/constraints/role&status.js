4a2f78c91a1f282e407c219f1a1d7e4bf690bb6f
nilpeep <niluferk038@gmail.com>
2024-04-16 23:05:34 +0300

    user router

--- src/constraints/role&status.js
+++ src/constraints/role&status.js
@@ -1,7 +1,7 @@
 module.exports = {
     role:{
         1:"admin",
-        2:"staff",
+        2:"accountant",
         3:"saler",
         4:"purchaser",
         5:"stockman"
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,19 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
 const router = require('express').Router()
-/* ------------------------------------------------------- */
+
+const user = require('../controllers/user')
+
+//* User router:
+
+router.route('/')
+.get(user.list)
+.post(user.create)
+
+router.route('/:userId')
+.get(user.read)
+.put(user.update)
+.patch(user.update)
+.delete(user.delete)
+
+module.exports = router
