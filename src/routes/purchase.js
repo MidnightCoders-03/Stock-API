ffed67a6050091199cc98ff96ade02c0bedf03cc
LeeDev26 <alid@paylinkzm.com>
2024-04-19 20:53:44 +0200

    Refactor purchase controller and routes

--- src/routes/purchase.js
+++ src/routes/purchase.js
@@ -12,13 +12,16 @@ const { isLogin, CRUD_A, C_AP, R_AP, U_AP } = require("../middlewares/permission
 router.use(isLogin)
 
 router.route("/")
-.get(R_AP, purchase.list)
-.post(C_AP, purchase.create)
+    .get(R_AP, purchase.list)
+    .post(C_AP, purchase.create)
+
+// router.route("/restore/:purchaseId")
+// .get(CRUD_A, purchase.restore)
 
 router.route("/:purchaseId")
-.get(R_AP, purchase.read)
-.put(U_AP, purchase.update)
-.patch(U_AP, purchase.update)
-.delete(CRUD_A, purchase.delete)
+    .get(R_AP, purchase.read)
+    .put(U_AP, purchase.update)
+    .patch(U_AP, purchase.update)
+    .delete(CRUD_A, purchase.delete)
 
 module.exports = router
